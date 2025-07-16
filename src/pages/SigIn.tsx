286b286471b7220f5b4c5e7fa513161d8c843706
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-07-16 14:12:05 -0300

    finish SignIn

--- src/pages/SigIn.tsx
+++ src/pages/SigIn.tsx
@@ -1,14 +1,31 @@
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
+import { useState } from "react"
+
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const [isLoading, setIsloading] = useState(false)
+  
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+
+    console.log(email, pwd)
+
+    setEmail("")
+    setPwd("")
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
        required
        name="E-mail"
        type="email"
        placeholder="seu@email.com"
+       onChange={(e) => setEmail(e.target.value)}
+       value={email}
       />
 
       <Input
@@ -16,9 +33,18 @@ export function SignIn() {
        name="Senha"
        type="password"
        placeholder="1234567"
+       onChange={(e) => setPwd(e.target.value)}
+       value={pwd}
       />
 
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>Entrar</Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-8 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar conta
+      </a>
     </form>
   )
 }
