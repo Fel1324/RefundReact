7e10b142f293cfb0bdcd3893e0e5a732c9188d69
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-07-16 13:58:41 -0300

    create Input and Button Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { Routes } from "./routes"
+
 export function App() {
   return (
-    <div>
-      <h1>Jesus eu te amo!</h1>
-    </div>
+    <Routes />
   )
 }
---
+++ src/routes/auth-routes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router"
+
+import { AuthLayout } from "../components/AuthLayout"
+import { SignIn } from "../pages/SigIn"
+
+export function AuthRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<SignIn />} />
+      </Route>
+    </Routes>
+  )
+}
