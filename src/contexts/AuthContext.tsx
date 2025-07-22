bf9b160e806cef69b843d4de0dd4a0ea70725106
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-07-22 20:53:18 -0300

    finish employee and manager routes features

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
-
 import { createContext, type ReactNode } from "react"
+
+import { api } from "../services/api"
 
 type AuthContext = {
   isLoading: boolean
@@ -20,6 +21,9 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   function save(data: UserAPIResponse) {
     localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.user))
     localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token)
+
+    api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`
+
     setSession(data)
   }
 
@@ -37,6 +41,8 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const token = localStorage.getItem(`${LOCAL_STORAGE_KEY}:token`)
 
     if(token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
       setSession({
         token,
         user: JSON.parse(user)
